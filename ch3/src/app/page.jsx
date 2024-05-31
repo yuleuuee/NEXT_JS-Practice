f2288d2efbba9d89cd284f200206907cbb99a3d0
Prabhav Gautam <yuleuuee@gmail.com>
2024-05-31 13:52:30 +0800

    problem solved : @ in the pathname

--- ch3/src/app/page.jsx
+++ ch3/src/app/page.jsx
@@ -1,19 +1,22 @@
-import LearnCssModule from "@/components/LearnCssModule";
-import ImageComponent from "@/components/ImageComponent";
-import LearnDataFetch from "@/components/LearnDataFetch";
+// import LearnDataFetch from "../components/LearnCssModule";
+
+// "@/" means "./src/" --> this helps in the quick path location
+import LearnCssModule from '@/components/LearnCssModule'
+import ImageComponent from '@/components/ImageComponent'
+import LearnDataFetch from '@/components/LearnDataFetch' 
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-evenly p-2">
+    <main className="flex flex-col first-line justify-evenly p-2">
+
       {/* <h1>Ch3 , Home Page</h1> */}
 
       {/* <LearnCssModule></LearnCssModule> */}
       
-      <ImageComponent></ImageComponent>
-      <ImageComponent></ImageComponent>
+      {/* <ImageComponent></ImageComponent> */}
 
-      {/* <LearnDataFetch></LearnDataFetch> */}
-
+      <LearnDataFetch></LearnDataFetch>
     </main>
   );
 }
--- ch3/src/components/LearnDataFetch.jsx
+++ ch3/src/components/LearnDataFetch.jsx
@@ -1,18 +1,27 @@
 async function getData(){
-    const res =await fetch("https://jsonplaceholder.typicode.com/posts") // By default is GET method
-    // if(!res.ok){
-    //     <p>DATA not found</p>
-    // }
+    const res = await fetch("https://jsonplaceholder.typicode.com/users") // By default is GET method
+
     return res.json()
 }
 
 const LearnDataFetch = async () => {
-    const data = await getData() // await : as it is an async 
-    console.log("DATA :",data);
+  const data = await getData() // await : as it is an async 
+  console.log("DATA :",data);
   return (
-    <div>
-      Learn Data Fetch
-    </div>
+    <>
+      <h1 className="font-bold">List of Users info. which are fetched form 'https://jsonplaceholder.typicode.com/users' :: </h1>
+
+      {
+        data.map((user,i)=>(
+          <div key={i} className="border border-black-50 m-4 p-2">
+            <h2>Name : {user.name}</h2>
+            <h2>Email : {user.email}</h2>
+            <h2>Contact no. : {user.phone}</h2>
+
+          </div>
+        ))
+      }
+    </>
   )
 }
 
